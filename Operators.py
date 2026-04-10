3e2633f52cd5602dd120063f96d6a4544ae3e909
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-04-10 14:21:38 +0600

    operator __ arithmetic ,assignment,relational,logical

---
+++ Operators.py
@@ -0,0 +1,40 @@
+
+#Arithmetic operator
+num1=100
+num2=40
+sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+remider=num1%num2
+print("The sum of",num1,"and",num2,"is",sum)
+print("The subtraction of",num1,"and",num2,"is",sub)
+print("The multiplication of",num1,"and",num2,"is",mul)
+print("The division of",num1,"and",num2,"is",div)
+print("The modulus  of",num1,"and",num2,"is",remider)
+
+#Relational operator
+a=20
+b=10
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+
+#Assignment operator
+x=10
+# x+=5 #x=x+5;
+x**=5
+print(x)
+
+
+#Logical operator
+a=10
+b=5
+
+print(a>5 and b>10)
+
+print(a>5 or b>10)
+print(not True)
+print(not False)
+print(not(a<b))
