c77bf3a46e0d7dc1a0f6328094690bec71420c6f
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-04-10 13:03:06 +0600

    data type __ String,Integer,Float,Boolean,NoneType__

--- typeOfvarible.py
+++ typeOfvarible.py
@@ -1,6 +1,10 @@
 Name="Gaurav Sen Gupto"
 Age=30
 CGPA=3.75
+information=True
+Type=None
 
 
 print("The name variable type is",type(Name),"\n The age variable type is ",type(Age),"\nThe CGPA variable type is",type(CGPA))
+print(type(information))
+print(type(Type))
