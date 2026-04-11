0fb4bb6f3f9c0a2bb85b49cee8296809c89a7d13
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-04-12 02:42:46 +0600

    odd even number find out

---
+++ oddEven.py
@@ -0,0 +1,10 @@
+number=int(input('Enter the number='))
+
+if(number%2==0):
+    print("Your given num ",number,"is Even")
+
+elif(number%2!=0):
+    print("Your given num",number,"is Odd")   
+
+else:
+    print("Not a number")
