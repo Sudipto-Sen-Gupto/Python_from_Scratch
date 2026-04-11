33ab2f5b360fdd7e0bea2c2a703abf4c7f6f0e8b
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-04-12 01:22:00 +0600

    string function

---
+++ String_Function.py
@@ -0,0 +1,27 @@
+#startsWith()
+Name="Gaurav"
+print(Name.lower().startswith("g"))
+
+#endswith()
+gmail="gauravsengupto@gmail.com"
+print(gmail.endswith("@gmail.com"))
+
+#replace()
+statement="I am learning javascript"
+replacing=statement.replace("javascript","python")
+print(replacing)
+
+#count()
+sentence="Dear David i am Dip .Do you know that i am learning python."
+countLetter=sentence.lower().count('d')
+print(countLetter)
+
+#capitalize
+constitution="the first constitution name in the world is MegnaCarta."
+capitalized=constitution.capitalize();
+print(capitalized)
+
+#find()
+manyWord="Find the word o"
+findWord=manyWord.find("o")
+print(findWord)
