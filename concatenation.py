3e878053944d545ca87838589f6fbcb7cb054788
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-04-11 22:28:54 +0600

    concatenation

---
+++ concatenation.py
@@ -0,0 +1,6 @@
+str1="I am learning python"
+str2="with apna college."
+str3="\n I also did this programming language previous.Now i do revise."
+final=str1+" "+str2+str3
+# print(str1+" "+str2)
+print(final)
