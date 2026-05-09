84be07758e0ada1d070f8bb139730f3a0a12e1cf
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2026-05-10 01:44:46 +0600

    calculate averege by using function

---
+++ avgThreeNumWithFunc.py
@@ -0,0 +1,8 @@
+
+def average(num1,num2,num3):
+    avg=(num1+num2+num3)/3
+    print('Average of three numbers',avg)
+    return avg
+average(4,5,7)
+average(28,25,43)
+average(29,27,38)
--- function.py
+++ function.py
@@ -4,3 +4,11 @@ def twoNum(a,b):
    return print('a=',a,"b=",b)
     
 twoNum(2,5)
+
+## add two number
+
+def addTwoNumber(a,b):
+   sum=a+b
+   print("Summation of two numbers=",sum)
+   return sum
+addTwoNumber(10,12)
